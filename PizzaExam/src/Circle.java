370a627f64ed46cb6f713382bb8439b029ce7874
hoon0602 <hoon0602@LAPTOP-OFE4UHIR>
2021-07-16 10:59:46 +0900

    PizzaExam

---
+++ PizzaExam/src/Circle.java
@@ -0,0 +1,8 @@
+
+public class Circle {
+	protected int size;
+	
+	public Circle(int size) { 
+		this.size = size;
+	}
+}
---
+++ PizzaExam/src/PizzaMain.java
@@ -0,0 +1,12 @@
+
+public class PizzaMain {
+
+	public static void main(String[] args) {
+		Pizza pizza1 = new Pizza(20, "페퍼로니");
+		Pizza pizza2 = new Pizza(18, "슈퍼수프림");
+		
+		System.out.println(pizza1.toString());
+		System.out.println(pizza2.toString());
+	}
+
+}
